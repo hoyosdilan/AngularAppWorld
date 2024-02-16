1f76d4539d9e2106d40f032c2564fc008131aaa8
Dilan Hoyos <dilan.hoyos@globant.com>
2024-02-15 20:50:53 -0500

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication, provideProtractorTestingSupport } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
+import routeConfig from "./app/routes";
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
+bootstrapApplication(AppComponent,
+  {providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)]})
+.catch(err => console.error(err));
